38311d449d4819749c6156f50318ab8f2819a372
ad03am <126007045+ad03am@users.noreply.github.com>
2023-06-10 17:40:15 +0200

    Add tests

--- Projekt/tests/recipe_tests.cpp
+++ Projekt/tests/recipe_tests.cpp
@@ -8,18 +8,30 @@ Ingredient ingredient2("Second ingredient", IngredientType::vegetable, 200);
 Ingredient ingredient3("Third ingredient", IngredientType::dairy, 300);
 Ingredient ingredient4("Fourth ingredient", IngredientType::grain, 130);
 
+Device device1("Blender", State::clean);
+Device device2("Pot", State::dirty);
+Device device3("Knife", State::clean);
+
 TEST_CASE("Recipe tests", "[recipe]") {
-    Recipe recipe("Test recipe", 20, Difficulty::easy, {ingredient1, ingredient2}, {});
+    Recipe recipe("Test recipe", 20, Difficulty::easy, {ingredient1, ingredient2}, {device1, device2});
     CHECK(recipe.getName() == "Test recipe");
     CHECK(recipe.getTime() == 20);
     CHECK(recipe.getDifficulty() == Difficulty::easy);
 
-    SECTION("Checking if vector is correct"){
+    SECTION("Checking if ingridient vector is correct"){
         CHECK(recipe.getIngredients()[0].getType() == IngredientType::meat);
         CHECK(recipe.getIngredients()[0].getCalories() == 100);
         CHECK(recipe.getIngredients()[1].getType() == IngredientType::vegetable);
         CHECK(recipe.getIngredients()[1].getCalories() == 200);
         CHECK(recipe.getIngredients().size() == 2);
+    }
+
+    SECTION("Checking if device vector is correct"){
+        CHECK(recipe.getDevices()[0].getName() == "Blender");
+        CHECK(recipe.getDevices()[0].getType() == State::clean);
+        CHECK(recipe.getDevices()[1].getName() == "Pot");
+        CHECK(recipe.getDevices()[1].getType() == State::dirty);
+        CHECK(recipe.getDevices().size() == 2);
     }
 
     SECTION("Setting new values") {
@@ -37,6 +49,11 @@ TEST_CASE("Recipe tests", "[recipe]") {
 
     SECTION("Setting recipe ingredients as an empty vector") {
         CHECK_THROWS_AS(recipe.setIngredients({}), NoIngredientsException);
+    }
+
+    SECTION("Setting recipe devices as an empty vector") {
+        recipe.setDevices({});
+        CHECK(recipe.getDevices().size() == 0);
     }
 
     SECTION("Adding ingredients to existing ones") {
